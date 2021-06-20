cc7d8a13ccfaaf83324619db61f93197185e2c49
Juan Ledesma <a01206568@itesm.mx>
2021-06-20 17:07:23 -0500

    Adding registering a new product logic

--- AmazonClone/src/main/kotlin/main.kt
+++ AmazonClone/src/main/kotlin/main.kt
@@ -1,5 +1,3 @@
-import kotlin.reflect.typeOf
-
 /*Amazon Clone is an app that will mimic the main functions that Amazon handles:
 1.- Be able to register an user
 2.- Be able to login an user
@@ -30,9 +28,14 @@ var registeredUsers = mutableMapOf<String, String>()
 //3.- For uploading a product
 var productName: String = ""
 var productCategory: String = ""
+var validCategories = setOf("clothes", "technology", "home", "food", "health")
 var productStatus: String = ""
-var productPrice: String = ""
+var validStatus = setOf("new", "pre-owned", "owned")
 var productDescription: String = ""
+var productPrice: Int = 0
+var productAddedCorrectly: Boolean = false
+//Consider substituting for an object. Simplifying implementation for now
+var productsList = arrayListOf<String>()
 
 //4.- For buying a product
 var productCart = listOf<String>()
@@ -57,7 +60,7 @@ var orderStatus: String = ""
 
 //Function to validate if user username and password are correct
 fun validCredentials(): Boolean {
-    var registeredPassword: String = ""
+    var registeredPassword = ""
 
     fun validUsername(): Boolean {
         //Check to see if the username exists in the registered users Map
@@ -105,79 +108,157 @@ fun validRegistration(): Boolean {
     return validateUsername() && validateEmail() && validatePasswords()
 }
 
-//TODO: Validate for nulls
-fun main() {
+//Function to check if a proper product was introduced
+fun validateProduct(): Boolean {
+    fun validCategory(): Boolean {
+        return if (validCategories.contains(productCategory.lowercase()))
+            true
+        else {
+            println("Please enter a valid category")
+            false
+        }
+    }
 
+    fun validStatus(): Boolean {
+        return if (validStatus.contains(productStatus.lowercase()))
+            true
+        else {
+            println("Please enter a valid status")
+            false
+        }
+    }
+
+    return validCategory() && validStatus()
+}
+
+//TODO: Validate for nulls
+//TODO: Optimize code
+//TODO: Validate input types (all read lines are strings now)
+//TODO: Validate for user entering numbers bigger than byte maximum (-127 to 127)
+fun main() {
+    var firstOption: Byte
     //Do while to keep the user iterating over the menu options till he decides to leave
     do {
-        //Show welcome menu
-        println("\nWelcome to <name in progress>!")
-        println("What do you want to do?")
-        println("1.- Login")
-        println("2.- Register")
-        println("3.- Exit")
-        var firstOption: Byte = readLine()?.toByte()!!
+        //Check to see if user has logged in
+        if (!session){
+            //Show welcome menu
+            println("\nWelcome to <name in progress>!")
+            println("What do you want to do?")
+            println("1.- Login")
+            println("2.- Register")
+            println("3.- Exit")
+            firstOption = readLine()?.toByte()!!
 
-        //Decision path: login, register or exit
-        when (firstOption) {
-            1.toByte() -> {
-                //Login path
-                if (registeredUsers.isEmpty()) {
-                    println("No users have been registered in the system. \nPlease register one first before attempting to login")
-                } else {
+            //Decision path: login, register or exit
+            when (firstOption) {
+                1.toByte() -> {
+                    //Login path
+                    if (registeredUsers.isEmpty()) {
+                        println("No users have been registered in the system. \nPlease register one first before attempting to login")
+                    } else {
+                        do {
+                            var secondOption: Byte
+                            println("Please enter your username")
+                            username = readLine()!!
+                            println("Please enter your password")
+                            password = readLine()!!
+                            //Validate user input: if everything is ok, set session variable to true
+                            session = validCredentials()
+                            if (session){
+                                println("Login successful! \nWelcome $username")
+                                break
+                            } else {
+                                println("Username or password are incorrect!")
+                                println("1.- Try again")
+                                println("2.- Return to main menu")
+                                secondOption = readLine()!!.toByte()
+                            }
+                        } while (secondOption != 2.toByte())
+                    }
+                }
+                2.toByte() -> {
                     do {
-                        var secondOption: Byte = 1
-                        println("Please enter your username")
-                        username = readLine()!!
-                        println("Please enter your password")
-                        password = readLine()!!
-                        //Validate user input: if everything is ok, set session variable to true
-                        session = validCredentials()
+                        //Register path
+                        var thirdOption: Byte
+                        println("Please enter an username")
+                        registrationUsername = readLine()!!
+                        println("Please enter an email")
+                        registrationEmail = readLine()!!
+                        println("Please enter a password")
+                        //Extra validation: Check user password structure
+                        registrationPassword = readLine()!!
+                        println("Please re-enter your password")
+                        registrationPasswordConfirmation = readLine()!!
+
+                        //If everything is ok, set session variable to true
+                        session = validRegistration()
                         if (session){
-                            println("Login successful! \nWelcome $username")
+                            //Add new user into the system
+                            registeredUsers[registrationUsername] = registrationPassword
+                            registeredEmails.add(registrationEmail)
+                            println("New user registered successfully! \nWelcome $registrationUsername")
                             break
                         } else {
-                            println("Username or password are incorrect!")
+                            println("1.- Try again")
+                            println("2.- Return to main menu")
+                            thirdOption = readLine()!!.toByte()
+                        }
+                    } while (thirdOption != 2.toByte())
+                }
+                3.toByte() -> firstOption = 3
+                else -> println("Please enter a valid option")
+            }
+        } else {
+            //Display articles list
+            println("\nWhat would you like to do?")
+            println("1.- Register an item")
+            println("2.- Buy an item")
+            println("3.- Exit")
+            firstOption = readLine()!!.toByte()
+
+            when(firstOption) {
+                1.toByte() -> {
+                    do {
+                        //Register path
+                        var secondOption: Byte
+                        //Extra validation: Check if product already exists
+                        println("Please enter the product name")
+                        productName = readLine()!!
+                        println("Please enter the product category")
+                        println("Available categories: clothes, technology, home, food or health")
+                        //Validate a valid category is entered
+                        productCategory = readLine()!!
+                        println("Please enter the product status")
+                        println("Product status options: new, pre-owned or used")
+                        //Validate a valid category is entered
+                        productStatus = readLine()!!
+                        println("Please enter the product description")
+                        //Extra validation: check for description length (i.e, 200 words)
+                        productDescription = readLine()!!
+                        println("Please enter the product price (in USD)")
+                        productPrice = readLine()?.toInt()!!
+
+                        //If everything is ok, set productAddedCorrectly variable to true
+                        productAddedCorrectly = validateProduct()
+                        if (productAddedCorrectly){
+                            //Add new product into the system
+                            productsList.add(productName)
+                            println("New product registered successfully!")
+                            break
+                        } else {
                             println("1.- Try again")
                             println("2.- Return to main menu")
                             secondOption = readLine()!!.toByte()
                         }
                     } while (secondOption != 2.toByte())
                 }
+                2.toByte() -> {
+                    //TODO: Define logic for buying a product
+                }
+                3.toByte() -> firstOption = 3
+                else -> println("Please enter a valid option")
             }
-            2.toByte() -> {
-                do {
-                    //Register path
-                    var thirdOption: Byte = 1
-                    println("Please enter an username")
-                    registrationUsername = readLine()!!
-                    println("Please enter an email")
-                    registrationEmail = readLine()!!
-                    println("Please enter a password")
-                    //Extra validation: Check user password structure
-                    registrationPassword = readLine()!!
-                    println("Please re-enter your password")
-                    registrationPasswordConfirmation = readLine()!!
-
-                    //If everything is ok, set session variable to true
-                    session = validRegistration()
-                    if (session){
-                        //Add new user into the system
-                        registeredUsers.put(registrationUsername, registrationPassword)
-                        registeredEmails.add(registrationEmail)
-                        println("New user registered successfully! \nWelcome $registrationUsername")
-                        break
-                    } else {
-                        println("1.- Try again")
-                        println("2.- Return to main menu")
-                        thirdOption = readLine()!!.toByte()
-                    }
-                } while (thirdOption != 2.toByte())
-            }
-            3.toByte() -> firstOption = 3
         }
-
-        //Display articles list
 
     } while (firstOption != 3.toByte())
 
