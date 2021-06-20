c18312600008a7783e6dc3e82337778b1a821f52
Juan Ledesma <a01206568@itesm.mx>
2021-06-20 13:03:24 -0500

    Adding login logic

--- AmazonClone/src/main/kotlin/main.kt
+++ AmazonClone/src/main/kotlin/main.kt
@@ -1,3 +1,5 @@
+import kotlin.reflect.typeOf
+
 /*Amazon Clone is an app that will mimic the main functions that Amazon handles:
 1.- Be able to register an user
 2.- Be able to login an user
@@ -22,8 +24,7 @@ var registrationPasswordConfirmation: String = ""
 var username: String = ""
 var password: String = ""
 var session: Boolean = false
-var registeredUsernames = listOf<String>()
-var registeredPasswords = listOf<String>()
+var registeredUsers = mutableMapOf<String, String>()
 
 //3.- For uploading a product
 var productName: String = ""
@@ -53,42 +54,92 @@ var shipDate: String = ""
 var trackingNumber: String = ""
 var orderStatus: String = ""
 
+//Function to validate if user username and password are correct
+fun validCredentials(): Boolean {
+    var registeredPassword: String = ""
 
+    fun validUsername(): Boolean {
+        //Check to see if the username exists in the registered users Map
+        return if (registeredUsers.containsKey(username)){
+            registeredPassword = registeredUsers.getValue(username)
+            true
+        } else
+            false
+    }
+
+    fun validPassword(): Boolean {
+        return registeredPassword == password
+    }
+
+    return validUsername() && validPassword()
+}
+
+//TODO: Validate for nulls
 fun main() {
-    //Show welcome menu
-    println("Welcome to <name in progress>!")
-    println("What do you want to do?")
-    println("1.- Login")    
-    println("2.- Register")
-    val firstOption: Byte = readLine()?.toByte()!!
 
-    //Login path
-    println("Please enter your username")
-    username = readLine()!!
-    println("Please enter your password")
-    password = readLine()!!
-    //Validate user input
-    //If everything is ok, set session variable to true
-    session = true
+    //Do while to keep the user iterating over the menu options till he decides to leave
+    do {
+        //Show welcome menu
+        println("\nWelcome to <name in progress>!")
+        println("What do you want to do?")
+        println("1.- Login")
+        println("2.- Register")
+        println("3.- Exit")
+        var firstOption: Byte = readLine()?.toByte()!!
 
-    //Register path
-    println("Please enter an username")
-    registrationUsername = readLine()!!
-    //Validate if username already exists
-    println("Please enter an email")
-    registrationEmail = readLine()!!
-    //Extra validation: Check for duplicate emails
-    println("Please enter a password")
-    //Extra validation: Check user password structure
-    registrationPassword = readLine()!!
-    println("Please re-enter your password")
-    registrationPasswordConfirmation = readLine()!!
-    //Validate if passwords match
-    //If everything is ok, set session variable to true
-    session = true
+        //Decision path: login, register or exit
+        when (firstOption) {
+            1.toByte() -> {
+                //Login path
+                if (registeredUsers.isEmpty()) {
+                    println("No users have been registered in the system. \nPlease register one first before attempting to login")
+                } else {
+                    do {
+                        var secondOption: Byte = 1
+                        println("Please enter your username")
+                        username = readLine()!!
+                        println("Please enter your password")
+                        password = readLine()!!
+                        //Validate user input: if everything is ok, set session variable to true
+                        session = validCredentials()
+                        if (session){
+                            println("Login successful! \n Welcome $username")
+                            break
+                        } else {
+                            println("Username or password are incorrect!")
+                            println("1.- Try again")
+                            println("2.- Return to main menu")
+                            secondOption = readLine()!!.toByte()
+                        }
+                    } while (secondOption != 2.toByte())
+                }
+            }
+            2.toByte() -> {
+                //Register path
+                println("Please enter an username")
+                registrationUsername = readLine()!!
+                //Validate if username already exists
+                println("Please enter an email")
+                registrationEmail = readLine()!!
+                //Extra validation: Check for duplicate emails
+                println("Please enter a password")
+                //Extra validation: Check user password structure
+                registrationPassword = readLine()!!
+                println("Please re-enter your password")
+                registrationPasswordConfirmation = readLine()!!
+                //Validate if passwords match
+                //If everything is ok, set session variable to true
+                session = true
+            }
+            3.toByte() -> firstOption = 3
+        }
 
-    //Display articles list
+        //Display articles list
 
+    } while (firstOption != 3.toByte())
+
+    //Goodbye message
+    println("Thanks for using our app")
 }
 
 
