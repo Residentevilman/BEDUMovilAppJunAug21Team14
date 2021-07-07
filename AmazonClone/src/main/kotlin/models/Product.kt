fca61cb5ae0faeb11f6f6c845f3d084d30308ceb
Residentevilman <raulvillegass@yahoo.com.mx>
2021-07-07 17:49:45 -0500

    Update Product.kt

--- AmazonClone/src/main/kotlin/models/Product.kt
+++ AmazonClone/src/main/kotlin/models/Product.kt
@@ -1,3 +1,7 @@
 package models
 
-class Product (val name: String, var category: String, var status: String, var description: String, var price: Float)
+class Product (val name: String, var category: String, var status: String, var description: String, var price: Float) {
+    fun productInformation(): String {
+        return "$name - $category - $status - ${description.take(15)}... - $$price"
+    }
+}
