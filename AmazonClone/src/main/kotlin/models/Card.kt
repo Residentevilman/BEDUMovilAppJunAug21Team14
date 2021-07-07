059d68fac13d742b260ad6fef37016d527e40b39
Residentevilman <raulvillegass@yahoo.com.mx>
2021-07-07 17:51:33 -0500

    Update Card.kt

--- AmazonClone/src/main/kotlin/models/Card.kt
+++ AmazonClone/src/main/kotlin/models/Card.kt
@@ -1,3 +1,3 @@
 package models
 
-class Card (val number: Long, val cardholderName: String, val expirationDate: String, val cvc: Byte)
+data class Card (val number: Long, val cardholderName: String, val expirationDate: String, val cvc: Byte)
